8eb8a878e47dea4aeab585b47ef1615ac3bfc9cc
ejieflorida128 <ejieflorida128@gmail.com>
2024-03-30 20:01:43 +0800

    [ejie]
    changes for March 30 2024

---
+++ Driver/dashboard.php
@@ -0,0 +1,76 @@
+<?php
+    include('../connnection/conn.php');
+    include('../includes/DriverSidebar.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="dashboard.css">
+
+</head>
+<body>
+    
+<div class="frame">
+        <div class="container">     
+                <div class="section1">
+                        <div class="box">
+                            <div class="title">
+                                    Profile
+                            </div>
+                            <div class="icon">
+                            <i class='bx bxs-user'></i>
+                            </div>
+                            <div class="des">
+                                Click here to proceed to profile..
+                            </div>
+                        </div>
+
+                        <div class="box">
+                            <div class="title">
+                                    My Schedule
+                            </div>
+                            <div class="icon">
+                            <i class='bx bxs-calendar'></i>
+                            </div>
+                            <div class="des">
+                            Click here to proceed to my schedule..
+                            </div>
+                        </div>
+                </div>
+                <div class="section2">
+                <div class="box">
+                            <div class="title">
+                                    Bus Schedules
+                            </div>
+                            <div class="icon">
+                            <i class='bx bxs-bus-school'></i>
+                            </div>
+                            <div class="des">
+                            Click here to proceed to my bus schedules..
+                            </div>
+                        </div>
+
+                        <div class="box">
+                            <div class="title">
+                                    Reports
+                            </div>
+                            <div class="icon">
+                            <i class='bx bxl-blogger'></i>
+                            </div>
+                            <div class="des">
+                            Click here to proceed to reports..
+                            </div>
+                        </div>
+                </div>
+        </div>
+</div>
+    
+</body>
+</html>
---
+++ includes/DriverSidebar.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Driver Page</title>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+
+    <style>
+        *{
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+  text-decoration: none;
+}
+:root {
+  --accent-color: #fff;
+  --gradient-color: #FBFBFB;
+}
+body{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+   width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #e3eefe 0%, #efddfb 100%);
+}
+
+.sidebar{
+  position: fixed;
+  width: 240px;
+  left: -240px;
+  height: 100%;
+  background-color: #fff;
+  transition: all .5s ease;
+}
+.sidebar header{
+  font-size: 28px;
+  color: #353535;
+  line-height: 70px;
+  text-align: center;
+  background-color: #fff;
+  user-select: none;
+  font-family: 'Lato', sans-serif;
+}
+.sidebar a{
+  display: block;
+  height: 65px;
+  width: 100%;
+  color: #353535;
+  line-height: 65px;
+  padding-left: 30px;
+  box-sizing: border-box;
+  border-left: 5px solid transparent;
+  font-family: 'Lato', sans-serif;
+  transition: all .5s ease;
+  text-decoration: none;
+  
+}
+.sidebar a:hover {
+  text-decoration: none;
+  color: white;
+}
+
+a.active,a:hover{
+  border-left: 5px solid var(--accent-color);
+  color: #fff;
+   background: linear-gradient(to left, var(--accent-color), var(--gradient-color));
+   text-decoration: none;
+}
+.sidebar a i{
+  font-size: 23px;
+  margin-right: 16px;
+}
+.sidebar a span{
+  letter-spacing: 1px;
+  text-transform: uppercase;
+}
+#check{
+  display: none;
+}
+label #btn,label #cancel{
+  position: absolute;
+  left: 5px;
+  cursor: pointer;
+  color: #d6adff;
+  border-radius: 5px;
+  margin: 15px 30px;
+  font-size: 29px;
+  background-color: #e8d1ff;
+  box-shadow:inset 2px 2px 2px 0px rgba(255,255,255,.5),
+    inset -7px -7px 10px 0px rgba(0,0,0,.1),
+   3.5px 3.5px 20px 0px rgba(0,0,0,.1),
+   2px 2px 5px 0px rgba(0,0,0,.1);
+  height: 45px;
+  width: 45px;
+  text-align: center;
+  text-shadow: 2px 2px 3px rgba(255,255,255,0.5);
+  line-height: 45px;
+  transition: all .5s ease;
+}
+label #cancel{
+  opacity: 0;
+  visibility: hidden;
+}
+#check:checked ~ .sidebar{
+  left: 0;
+}
+#check:checked ~ label #btn{
+  margin-left: 245px;
+  opacity: 0;
+  visibility: hidden;
+}
+#check:checked ~ label #cancel{
+  margin-left: 245px;
+  opacity: 1;
+  visibility: visible;
+}
+@media(max-width : 860px){
+  .sidebar{
+    height: auto;
+    width: 70px;
+    left: 0;
+    margin: 100px 0;
+  }
+  header,#btn,#cancel{
+    display: none;
+  }
+  span{
+    position: absolute;
+    margin-left: 23px;
+    opacity: 0;
+    visibility: hidden;
+  }
+  .sidebar a{
+    height: 60px;
+  }
+  .sidebar a i{
+    margin-left: -10px;
+  }
+  a:hover {
+    width: 200px;
+    background: inherit;
+    text-decoration: none;
+  }
+  .sidebar a:hover span{
+    opacity: 1;
+    visibility: visible;
+    text-decoration: none;
+  }
+}
+
+.sidebar > a.active,.sidebar > a:hover:nth-child(even) {
+  --accent-color: #52d6f4;
+  --gradient-color: #c1b1f7;
+}
+.sidebar a.active,.sidebar > a:hover:nth-child(odd) {
+  --accent-color: #c1b1f7;
+  --gradient-color: #A890FE;
+}
+
+
+.frame {
+  width: 50%;
+  height: 30%;
+  margin: auto;
+  text-align: center;
+}
+
+h2 {
+  position: relative;
+  text-align: center;
+  color: #353535;
+  font-size: 60px;
+  font-family: 'Lato', sans-serif;
+  margin: 0;
+  color: #a759f5;
+}
+
+p {
+  font-family: 'Lato', sans-serif;
+  font-weight: 300;
+  text-align: center;
+  font-size: 30px;
+  color: #d6adff;
+  margin: 0;
+}
+
+    </style>
+</head>
+<body>
+<input type="checkbox" id="check">
+    <label for="check">
+      <i class="fas fa-bars" id="btn"></i>
+      <i class="fas fa-times" id="cancel"></i>
+    </label>
+    <div class="sidebar">
+      <header>Menu</header>
+      <a href="#" class="active">
+        <i class="fas fa-qrcode"></i>
+        <span>Dashboard</span>
+      </a>
+      <a href="#">
+      <i class='bx bxs-user'></i>
+        <span>Profile</span>
+      </a>
+      <a href="#">
+      <i class='bx bxs-calendar'></i>
+        <span>My schedule</span>
+      </a>
+      <a href="#">
+        <i class='bx bxs-bus-school'></i>
+        <span>Bus Schedules</span>
+      </a>
+      <a href="#">
+      <i class='bx bxl-blogger'></i>
+        <span>Reports</span>
+      </a>
+      <a href="../index.php" style = "margin-top: 140px;">
+      <i class='bx bxs-door-open' ></i>
+        <span>Logout</span>
+      </a>
+     
+    </div>
+</body>
+    
+  <!-- <div class="frame">
+<p> Responsive </p>
+<h2>SIDE BAR</h2>
+  <p>in Pure CSS</p>
+
+</div>   -->
+</body>
+</html>
